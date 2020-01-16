54d6a8670e81804b52ab9bd12e449f3a278477a5
IkezoeMakoto <150283t@gmail.com>
2020-01-16 22:44:26 +0900

    refactor: 関数に切り分け、locationと違うリンクはtarget=_blankにした

--- script.js
+++ script.js
@@ -1,29 +1,56 @@
-window.addEventListener("load", main, false);
+$(() => {
+    window.addEventListener("load", main, false);
 
-function main(e) {
-    const jsInitCheckTimer = setInterval(jsLoaded, 1000);
+    function main(e) {
+        const jsInitCheckTimer = setInterval(jsLoaded, 1000);
 
-    function jsLoaded() {
-        if (document.querySelector('a') != null) {
-            clearInterval(jsInitCheckTimer);
-            const $origin = $("a[href^=https\\:\\/\\/sample\\.jp]");
-            const $https = $("a[href^=https\\:\\/\\/]");
-            const $http = $("a[href^=http\\:\\/\\/]");
-            $https.not($origin).each((_, e) => {
-                addTargetBlank(e);
-            });
-            $http.not($origin).each((_, e) => {
-                addTargetBlank(e);
-            });
+        function jsLoaded() {
+            if (document.querySelector('a') != null) {
+                clearInterval(jsInitCheckTimer);
+
+                const $origin = $(selectorEscape(aTagSelector(httpsUrl(getHost()))));
+                const $https = $(selectorEscape(aTagSelector(httpsUrl())));
+                const $http = $(selectorEscape(aTagSelector(httpUrl())));
+                $https.not($origin).each((_, e) => {
+                    addTargetBlank(e);
+                });
+                $http.not($origin).each((_, e) => {
+                    addTargetBlank(e);
+                });
+            }
         }
     }
-}
 
-/**
- * @param e element
- */
-addTargetBlank = (e) => {
-    // 削除してから追加しないと正しく追加されない
-    $(e).removeAttr('target');
-    $(e).attr('target', '_blank');
-};
+    aTagSelector = (s) => {
+        return 'a[href^=' + s + ']';
+    };
+
+    httpUrl = (host) => {
+        return 'http://' + host;
+    };
+
+    httpsUrl = (host) => {
+        return 'https://' + host;
+    };
+
+    getHost = () => {
+        return location.host;
+    };
+
+    /**
+     * @param e element
+     */
+    addTargetBlank = (e) => {
+        // 削除してから追加しないと正しく追加されない
+        $(e).removeAttr('target');
+        $(e).attr('target', '_blank');
+    };
+
+    /**
+     * @param {string} s
+     * @returns {string}
+     */
+    selectorEscape = (s) => {
+        return s.replace(/[ !"#$%&'()*+,.\/:;<=>?@\[\\\]^`{|}~]/g, '\\$&');
+    };
+});
